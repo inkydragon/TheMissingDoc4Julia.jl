3c321680c40ba0c92f896fff524e3a8156854097
Chengyu HAN <git@wo-class.cn>
2024-03-03 16:02:00 -0600

    doc: split sidebar pages to standalone file

---
+++ docs/sidebar.jl
@@ -0,0 +1,60 @@
+"""
+All Sidebar Pages.
+
+## ref
+- https://documenter.juliadocs.org/stable/man/guide/#Pages-in-the-Sidebar
+"""
+
+all_pages = [
+    "Home" => "index.md",
+    "Mathematics" => Any[
+        "Mathematics" => "Mathematics.md",
+        "Arithmetic" => "Base.Math/Arithmetic/Arithmetic.md",
+        "Trigonometry" => Any[
+            "Trigonometry" => "Base.Math/Trigonometry/Trigonometry.md",
+            "Sine" => Any[
+                "Base.Math/Trigonometry/Sine/sin.md",
+                "Base.Math/Trigonometry/Sine/sind.md",
+                "Base.Math/Trigonometry/Sine/sinpi.md",
+                "Base.Math/Trigonometry/Sine/sinh.md",
+                "Base.Math/Trigonometry/Sine/sincos.md",
+                "Base.Math/Trigonometry/Sine/sincosd.md",
+                "Base.Math/Trigonometry/Sine/sincospi.md",
+                "Base.Math/Trigonometry/Sine/asin.md",
+                "Base.Math/Trigonometry/Sine/asind.md",
+                "Base.Math/Trigonometry/Sine/asinh.md",
+            ],
+            "Cosine" => "Base.Math/Trigonometry/Cosine.md",
+            "Tangent" => "Base.Math/Trigonometry/Tangent.md",
+            "Cotangent" => "Base.Math/Trigonometry/Cotangent.md",
+            "Secant" => "Base.Math/Trigonometry/Secant.md",
+            "Cosecant" => "Base.Math/Trigonometry/Cosecant.md",
+            "Hypotenuse" => "Base.Math/Trigonometry/Hypotenuse.md",
+            "Conversions" => "Base.Math/Trigonometry/Conversions.md",
+        ],
+        "Exponents and Logarithms" => Any[
+            "Base.Math/Exp-and-Log/exp-and-log.md",
+            "Base.Math/Exp-and-Log/log.md",
+        ],
+        "Complex Numbers" => "Base.Math/Complex/Complex.md",
+    ],
+    "Stdlib" => Any[
+        "stdlib/RandomNumbers.md",
+        "stdlib/LinearAlgebra.md",
+        "stdlib/SparseArrays.md",
+    ],
+    hide("Julia Packages" => "Packages/FindPackages.md", [
+        "Packages/Interpolation.md",
+        "Packages/Optimization.md",
+        "Packages/Integration-and-Differential.md",
+        "Packages/FourierTransform-and-Filtering.md",
+        "Packages/Graph-and-Network.md",
+        "Packages/Geometry.md",
+        "Packages/Quantum.md",
+    ]),
+    # TODO: maybe move those to other project
+    "CheatSheet" => Any[
+        "SciPy" => "cheatsheet/scipy.md",
+        "Matlab" => "cheatsheet/matlab.md",
+    ],
+] # all_pages
